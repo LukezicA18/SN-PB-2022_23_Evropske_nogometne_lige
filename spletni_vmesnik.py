2f33c5ea8b4f306eb9a8cc2dd68df4be64d9853e
Aljaž <aljaz.lukezic@gmail.com>
2022-12-19 13:49:29 +0100

    dodal nove stvari

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -3,6 +3,14 @@ import model
 
 glavni_model = model.Model()
 
+@bottle.route("/static/slikice/<filename>")    #ZA SLIKO
+def serve_static_files(filename):
+    return bottle.static_file(filename, root="./static/img")  
+
+@bottle.route("/static/css/<filename>")    #ZA RDEČO barvo, dodamo to kar mamo na datoteki style.css
+def serve_static_files(filename):
+    return bottle.static_file(filename, root="./static/css") 
+
 @bottle.get("/")
 def glavna_stran():
     
